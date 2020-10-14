20f3abe392084ce56020fe5783460ed98b4e6013
AnwarH <anwar-360@hotmail.com>
2020-10-14 19:10:48 +0000

    pasted scripts again

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -2,7 +2,7 @@
 
 mkdir -p /var/backups/inotify
 if echo $1 | grep -qP "\..*\.sw(p|x|px)$" ; then #for vim users:)
-	exit 0
+        exit 0
 fi
-cp -p --parents $1 /var/backups/inotify
-mv /var/backups/inotify$1 /var/backups/inotify$1_$(date + '%Y.%m.%d_%H:%M:%S')
+cp -p --parents $1  /var/backups/inotify
+mv /var/backups/inotify$1 /var/backups/inotify$1_$(date +'%Y.%m.%d_%H:%M:%S')
